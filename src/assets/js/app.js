06fda005d0ac8388414e1bea72b45001353bbac7
Adil Heybetov <heybetov1998@gmail.com>
2023-04-07 01:27:08 +0400

    add product ready

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,10 @@
 const products = document.querySelector("#products");
+const nameInput = document.querySelector("#productName");
+const priceInput = document.querySelector("#productPrice");
+const linkInput = document.querySelector("#imageSrc");
+const addButton = document.querySelector("#addNewProduct");
 
+let id = 4;
 const DUMMY_PRODUCTS = [
     {
         id: 1,
@@ -11,23 +16,25 @@ const DUMMY_PRODUCTS = [
         id: 2,
         name: "Product 2",
         src: "src/assets/images/prod2.jpeg",
-        price: 20,
+        price: 10,
     },
     {
         id: 3,
         name: "Product 3",
         src: "src/assets/images/prod3.jpeg",
-        price: 30,
+        price: 10,
     },
     {
         id: 4,
         name: "Product 4",
         src: "src/assets/images/prod4.webp",
-        price: 30,
+        price: 10,
     },
 ];
 
 const render = () => {
+    products.innerHTML = "";
+
     for (const product of DUMMY_PRODUCTS) {
         createProduct(product);
     }
@@ -106,5 +113,24 @@ const createCardTitleElement = (product) => {
 
     return cardTitle;
 };
+
+const addNewProduct = () => {
+    DUMMY_PRODUCTS.push({
+        id: ++id,
+        name:
+            nameInput.value === "" ? `no_name_product_${id}` : nameInput.value,
+        src:
+            linkInput.value !== ""
+                ? linkInput.value
+                : "src/assets/images/noimage.webp",
+        price: priceInput.value === "" ? 0 : +priceInput.value,
+    });
+
+    render();
+};
+
+const search = () => {};
+
+addButton.addEventListener("click", addNewProduct);
 
 render();
