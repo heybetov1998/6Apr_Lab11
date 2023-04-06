41e495dd293f516f754d5215e6473c8e957c7403
Adil Heybetov <heybetov1998@gmail.com>
2023-04-06 17:44:05 +0400

    added add new product section

---
+++ src/assets/js/app.js
@@ -0,0 +1,26 @@
+const DUMMY_PRODUCTS = [
+    {
+        id: 1,
+        name: "Product 1",
+        src: "../images/prod1.jpg",
+        price: 10,
+    },
+    {
+        id: 2,
+        name: "Product 2",
+        src: "../images/prod2.jpeg",
+        price: 20,
+    },
+    {
+        id: 3,
+        name: "Product 3",
+        src: "../images/prod3.jpeg",
+        price: 30,
+    },
+    {
+        id: 4,
+        name: "Product 4",
+        src: "../images/prod4.jpeg",
+        price: 30,
+    },
+];
